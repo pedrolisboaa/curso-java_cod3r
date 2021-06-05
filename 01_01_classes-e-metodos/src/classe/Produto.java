49ee75296cffc82930d0b9145d748d0fc7177c30
Pedro Lisboa <phlisboa2000@gmail.com>
2021-06-05 18:16:50 -0300

    atualização curso

---
+++ 01_01_classes-e-metodos/src/classe/Produto.java
@@ -0,0 +1,27 @@
+package classe;
+
+public class Produto {
+
+	public String nome;
+	public Double preco;
+	public Double desconto;
+	
+	public Produto() {
+		
+	}
+
+	public Produto(String nome, Double preco, Double desconto) {
+		this.nome = nome;
+		this.preco = preco;
+		this.desconto = desconto;
+	}
+	
+	
+	public Double precoComDesconto() {
+		return preco * (1 - desconto);
+	}
+	
+	public Double precoComDesconto(double descontoGerente) {
+		return preco * (1 - (desconto + descontoGerente)); 
+	}
+}
